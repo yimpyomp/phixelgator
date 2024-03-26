11f9be7facee542e77ebaec0a5cef6ab89b8475c
yimpyomp <sebastian.enzi@gmail.com>
2024-03-26 16:58:29 -0500

    More minor formatting changes to reflect PEP8 standards, or at least whatever my IDE considers to be PEP8 Remove functionality for both HLS and HSV, neither work for Python 3.10

--- phixelgator.py
+++ phixelgator.py
@@ -4,38 +4,13 @@ import sys, argparse, math, json, os, colorsys
 from PIL import Image
 
 
-# TODO: Still does not work when using hls or hsv. How to fix? I do not know
-""" These convert from decimal back to 0-255 mode """
-
-
-def hls_to_rgb(h, l, s):
-    return map(lambda x: int(x * 255.0), colorsys.hls_to_rgb(h, l, s))
-
-
-def hsv_to_rgb(h, s, v):
-    return map(lambda x: int(x * 255.0), colorsys.hsv_to_rgb(h, s, v))
-
-
-def rgb_to_hsv(r, g, b):
-    return colorsys.rgb_to_hsv(r / 255, g / 255, b / 255)
-
-
-def rgb_to_hls(r, g, b):
-    return colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
-
-
 """ TODO: index colors via bitshift representation
     rather than hex conversion """
 
 
 def get_hex(color, mode='rgb'):
     """Get color hex value from rgb (or rgba)"""
-    if 'hsv' == mode:
-        rgb = hsv_to_rgb(*(color[:3]))
-    elif 'hls' == mode:
-        rgb = hls_to_rgb(*(color[:3]))
-    else:
-        rgb = color[:3]
+    rgb = color[:3]
     return ''.join(map(lambda t: hex(int(t)).split('x', 1)[1], rgb))
 
 
@@ -44,29 +19,16 @@ def color_diff(c1, c2):
     return sum(map(lambda x: (x[0] - x[1]) ** 2, list(zip(c1[:3], c2[:3]))))
 
 
-def color_diff_wheighted(c1, c2, mode='hsv'):
-    """HSV and HLS should have different weights... TODO: decide what they are :P"""
-    diff_pix = map(lambda x: abs(x[0] - x[1]), list(zip(c1[:3], c2[:3])))
-    return (diff_pix[0] * 10) + (diff_pix[1] * 10) + (diff_pix[2] * 10)
-
-
 def average_pixel(data, mode='rgb'):
     """Takes a list of pixel data tuples and finds average."""
-    if 'rgb' == mode:
-        return list(map(lambda x: int(round(sum(x) / len(data))), list(zip(*data))[:3]))
-    else:
-        map_size = len(list(zip(*data))[:3])
-        return list(map(lambda x: sum(x) / map_size, list(zip(*data))[:3]))
+    return list(map(lambda x: int(round(sum(x) / len(data))), list(zip(*data))[:3]))
 
 
 def get_closest_color(color, palette, hexdict, mode='rgb'):
     """Find the closest color in the current palette. TODO: optimize!"""
     hexval = get_hex(color, mode)
     if hexval not in hexdict:
-        if mode != 'rgb':
-            diff_func = color_diff_wheighted
-        else:
-            diff_func = color_diff
+        diff_func = color_diff
         hexdict[hexval] = min(palette, key=lambda c: diff_func(color, c))
     return list(hexdict[hexval])  # "list" looks redundant, but we want a *copy* of the color
 
@@ -75,13 +37,10 @@ def get_closest_color(color, palette, hexdict, mode='rgb'):
     perhaps with numpy """
 
 
-def phixelate(img, palette, blockSize, mode='rgb'):
+def phixelate(img, palette, blockSize):
     """Takes a PIL image object, a palette, and a block-size and alters colors in-place. no return val."""
     width, height = img.size
     rgb = img.load()
-    if mode == 'HSV':
-        thing = img.convert('HSV').load()
-        print(type(thing))
     blockWidth = int(math.ceil(width / blockSize))
     blockHeight = int(math.ceil(height / blockSize))
     hexdict = {}  # store "closest" colors to avoid repeat computations.
@@ -103,23 +62,10 @@ def phixelate(img, palette, blockSize, mode='rgb'):
             # alpha isn't used in finding the color so just pop it off for later
             avg_alpha = int(round(sum(list(zip(*container))[3]) / len(container)))
 
-            """ TODO: store converted RGB values to prevent duplicate
-          calls to colorsys """
-            if 'hsv' == mode:
-                container = map(lambda co: rgb_to_hsv(*(co[:3])), container)
-            if 'hls' == mode:
-                container = map(lambda co: rgb_to_hls(*(co[:3])), container)
-
             # Convert a block to one color -- take the average, and find the closest palette color
-            color = average_pixel(container, mode)
+            color = average_pixel(container)
             if palette:
-                color = get_closest_color(color, palette, hexdict, mode)
-
-            # Convert back to rgb if we're in hsv or hls mode
-            if 'hsv' == mode:
-                color = list(hsv_to_rgb(*color))
-            if 'hls' == mode:
-                color = list(hls_to_rgb(*color))
+                color = get_closest_color(color, palette, hexdict)
 
             # stick alpha channel back on and convert to tuple
             color.append(avg_alpha)
@@ -134,12 +80,7 @@ def phixelate(img, palette, blockSize, mode='rgb'):
 
 def generate_palette(img, mode='rgb'):
     """Generate a palette json file from an image. Image should NOT have an alpha value!"""
-    if 'hsv' == mode:
-        transform = lambda _, rgb: list(rgb_to_hsv(*rgb))
-    elif 'hls' == mode:
-        transform = lambda _, rgb: list(rgb_to_hls(*rgb))
-    else:
-        transform = lambda _, rgb: list(rgb)
+    transform = lambda _, rgb: list(rgb)
     return json.dumps(map(transform, img.getcolors(img.size[0] * img.size[1])))
 
 
@@ -184,9 +125,6 @@ if __name__ == "__main__":
     parse.add_argument('-x', '--crop', choices=['tl', 'tr', 'bl', 'br'],
                        help="If this flag is set, the image will be cropped to conform to the Block Size. "
                             "The argument passed describes what corner to crop from.")
-    parse.add_argument('-m', '--mode', choices=['rgb', 'hsv', 'hls'], default='rgb',
-                       help="The color mode to use. hsv or hls may produce more desirable results than the default rgb "
-                            "but the process will take longer.")
     parse.add_argument('-g', '--generate', action='store_true',
                        help="This flag overrides the default behaviour of "
                              "infile and outfile options -- instead of "
@@ -217,12 +155,7 @@ if __name__ == "__main__":
     if args.custom is not None:
         palette = json.loads(args.custom.read())
         args.custom.close()
-        # To simplify things, the custom palette generator only makes rgb files,
-        # so it's fairly safe to assume that's what we're getting.
-        if 'hsv' == args.mode:
-            palette = map(lambda rgb: rgb_to_hsv(*rgb), palette)
-        elif 'hls' == args.mode:
-            palette = map(lambda rgb: rgb_to_hls(*rgb), palette)
+
     elif args.palette is not None:
         try:
             path = os.sep.join([os.path.dirname(os.path.realpath(__file__)), 'palettes', args.mode, args.palette])
@@ -237,7 +170,7 @@ if __name__ == "__main__":
     if args.crop:
         img = phixel_crop(img, args.block, args.crop)
 
-    phixelate(img, palette, args.block, args.mode)
+    phixelate(img, palette, args.block)
 
     """ Try to resize the image and fail gracefully """
     if args.dimensions:
